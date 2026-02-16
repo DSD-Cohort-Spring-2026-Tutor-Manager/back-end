18564e9bdc7978fa656e20303e21c2784d9f30d3
Ankit Shah <shahankitjava@users.noreply.github.com>
2026-02-15 19:08:50 -0600

    chore: #1 Update database details readme (#12)
    Signed-off-by: Ankit Shah <shahankitjava@users.noreply.github.com>
    Co-authored-by: Suraj00300 <Surajraaj.kandel@gmail.com>
    Co-authored-by: skandel1 <suraj.kandel@sparkintell.com>
    Co-authored-by: Padmaja Ramesh <padmajaramesh1205@gmail.com>
    Co-authored-by: padmaja-ramesh <padmaja-ramesh@users.noreply.github.com>

--- src/main/java/org/tutortoise/service/TutortoiseApplication.java
+++ src/main/java/org/tutortoise/service/TutortoiseApplication.java
@@ -1,4 +1,4 @@
-package org.tutorial.demo;
+package org.tutortoise.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TutortoiseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TutortoiseApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(TutortoiseApplication.class, args);
+  }
 
 }
