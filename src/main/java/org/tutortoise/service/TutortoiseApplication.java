d1632c13e2a9f2c44e9f4a8b253a9441a5965e96
Padmaja Ramesh <padmajaramesh1205@gmail.com>
2026-02-16 13:38:55 -0500

    feat: #0 Dockerfile image yml (#29)
    Signed-off-by: Ankit Shah <shahankitjava@users.noreply.github.com>
    Signed-off-by: Padmaja Ramesh <padmajaramesh1205@gmail.com>
    Co-authored-by: San-ta-Fe <199674528+San-ta-Fe@users.noreply.github.com>
    Co-authored-by: Suraj00300 <Surajraaj.kandel@gmail.com>
    Co-authored-by: skandel1 <suraj.kandel@sparkintell.com>
    Co-authored-by: padmaja-ramesh <padmaja-ramesh@users.noreply.github.com>
    Co-authored-by: Ankit Shah <shahankitjava@users.noreply.github.com>

---
+++ src/main/java/org/tutortoise/service/TutortoiseApplication.java
@@ -0,0 +1,13 @@
+package org.tutortoise.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutortoiseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TutortoiseApplication.class, args);
+  }
+
+}
