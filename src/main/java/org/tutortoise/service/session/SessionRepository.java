f104d91d1641f6394b98a0f99888e64b932a376f
skandel1 <suraj.kandel@sparkintell.com>
2026-02-16 18:58:59 -0600

    session service created

---
+++ src/main/java/org/tutortoise/service/session/SessionRepository.java
@@ -0,0 +1,11 @@
+package org.tutortoise.service.session;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SessionRepository extends JpaRepository<Session, Integer> {
+    List<Session> findByTutorTutorId(Integer tutorId);
+}
