b8d55856426c88a0631b1870d682f4b2c899ff34
shahankitjava <shahankit.java@gmail.com>
2026-02-18 18:57:38 -0600

    resolved conflicts

--- src/main/java/org/tutortoise/service/TutortoiseApplication.java
+++ src/main/java/org/tutortoise/service/TutortoiseApplication.java
@@ -1,4 +1,8 @@
+<<<<<<<< HEAD:src/main/java/org/tutortoise/service/TutortoiseApplication.java
 package org.tutortoise.service;
+========
+package org.tutorial.demo;
+>>>>>>>> e23037b (chore: #2 Sync Integration Branch with Main (#18)):src/main/java/org/tutorial/demo/TutortoiseApplication.java
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
