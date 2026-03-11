893d6ffc468dbb0e04c6993c22d9ea97935c233b
Eric Bush <erbush01@gmail.com>
2026-03-11 18:12:29 -0400

    Mark passed sessions as cancelled, order by date asc for open sessions

--- src/main/java/org/tutortoise/service/session/SessionRepository.java
+++ src/main/java/org/tutortoise/service/session/SessionRepository.java
@@ -67,13 +67,6 @@ public interface SessionRepository extends JpaRepository<Session, Integer> {
             nativeQuery = true)
     void setPastSessionsAsCancelled();
 
-    @Query(
-            value =
-                    "SELECT * FROM session WHERE student_id_fk is NULL " +
-                            "AND session_status = 'open' " +
-                            "AND datetime_started > CURRENT_TIMESTAMP " +
-                            "ORDER BY datetime_started ASC",
-            nativeQuery = true)
     Optional<Session> findBySessionIdOrderByDatetimeStartedDesc(Integer sessionId);
 
 
